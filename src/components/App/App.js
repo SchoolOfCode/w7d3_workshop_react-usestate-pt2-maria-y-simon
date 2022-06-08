02f1b226a7bf054d996c0602126b3261f72f6db2
maroaraujo <tiagomoller@tiagos-mbp.home>
2022-06-08 16:58:27 +0100

    trying to get the values

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+//import React from "react";
+import {useState} from "react"; 
 import "./App.css";
+import Item from "../Item";
+import Input from "../Input";
 
+// PLAN
+// To get the text written on Input field
+// and add this text into the li -> Item
+// 
 function App() {
+  const [text, setText] = useState("");
+
+  //This function needs to get the input and add to list
+  function handleChange(){
+setText(text+Input.value)
+console.log(Input.value)
+  }
+
+
   return (
     <div className="App">
       <h1>JSX will go here!</h1>
+      <Input value={newText} onChange={function(){handleChange(newText)}}/>
+
+      <Item/>
+
     </div>
   );
 }
---
+++ src/components/Input/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Input(text){
+    return <input type="text">{text}</input>
+};
+
+export default Input;
